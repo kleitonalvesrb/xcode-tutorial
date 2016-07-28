7bd50daf13a221e52853a2d11a0148f98285742e
Kleiton Batista <kleiton.market@gmail.com>
2016-07-28 16:46:55 -0300

    adi o segundo print

--- git/ViewController.swift
+++ git/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("oi")
+        print("add segundo print")
     }
 
     override func didReceiveMemoryWarning() {
